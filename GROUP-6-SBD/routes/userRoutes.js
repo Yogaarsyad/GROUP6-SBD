0ec1e250497f5934daaa08a3e47c95bdb1ef72ee
Yogaarsyad <08trioga08@gmail.com>
2025-05-15 12:15:49 +0700

    Update lagi

--- GROUP-6-SBD/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.post('/', userController.createUser);
-router.get('/:id', userController.getUser);
-router.post('/follow', userController.followUser);
-
-module.exports = router;
